1cf8946600acdf86d627fefb06fefe43b457ad16
Matheus <matheus.nobregamts@gmail.com>
2023-11-02 22:45:34 -0300

    jogo funcionando (quase finalizado)

--- main2.js
+++ main2.js
@@ -5,11 +5,58 @@ const personagens = [
     'barbabranca','doffy','franky','katakuri','luffy','nami','robin','zoro',
 ]
 
+const embaralhada = personagens.sort(() => Math.random() - 0.5);
+
 let pers1 = '';
 let pers2 = '';
 
+const revelar = ({target}) => { 
+    if (target.parentNode.className.includes('revelar')) {
+        return;
+      }
+
+    if (pers1 === '') {
+
+        target.parentNode.classList.add('revelar');
+        pers1 = target.parentNode;
+    
+      } else if (pers2 === '') {
+    
+        target.parentNode.classList.add('revelar');
+        pers2 = target.parentNode;
+    
+        comparar();
+    
+    }}
+
+const comparar = () => {
+    const carta1 = pers1.getAttribute('data-personagens');
+    const carta2 = pers2.getAttribute('data-personagens');
+    if (carta1 === carta2){
+        pers1.firstChild.classList.add('efeito');
+        pers2.firstChild.classList.add('efeito');
+        pers1 = '';
+        pers2 = '';
+    } else {
+        setTimeout(() => {
+            pers1.classList.remove('revelar');
+            pers2.classList.remove('revelar');
+            pers1 = '';
+            pers2 = '';
+        }, 500);
+  }
+
+}
+    
+
+
+
+
+
+
+
 // criando uma função para criar as divs que vão conter as partes da frente e de traz das cartas
-function createcard(personagem){
+function criarcarta(personagem){
     const card = document.createElement('div');
     const frent = document.createElement('div');
     const atras = document.createElement('div');
@@ -21,14 +68,11 @@ function createcard(personagem){
     frent.style.backgroundImage = `url(Imagens/${personagem}.jpg`;
     
     //criando ouvinte de evento, nesse caso um evento por clique 
-    card.addEventListener('click', function(){
-        if(pers1 == ''){
-            card.style.transform = 'rotateY(180deg)'
-            pers1 = card
-        } else if (pers2 == ''){
-            card.style.transform = 'rotateY(180deg)'
-            pers2 = card
-        }})
+    
+    card.addEventListener('click', revelar);
+
+      
+        
    
 
     card.appendChild(frent);
@@ -43,14 +87,10 @@ function createcard(personagem){
 }
 
 
-function cardschosen(){
-    if (pers1 === pers2){
-        alert('ola');
-}}
 
 function loadgame() {
-    personagens.forEach(function(personagem) {
-        const card = createcard(personagem);
+    embaralhada.forEach(function(personagem) {
+        const card = criarcarta(personagem);
         grid.appendChild(card);
     });
 }
